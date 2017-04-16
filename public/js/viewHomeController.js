c8da0809a7feff74b2bb1a0d6d0ff7218df871b8
wengjunxiao <lwengjunxiao@hotmail.com>
2017-04-16 15:49:45 +0800

    init

--- public/js/viewHomeController.js
+++ public/js/viewHomeController.js
@@ -8,10 +8,10 @@ define(['angular', 'app'], function (angular, app) {
 		var currIndex = 0;
 		$scope.myInterval = 5000;
         $scope.slides = [
-            {text: 'one', image: '/img/酒窖.png', id: currIndex++},
-            {text: 'two', image: '/img/旅行.png', id: currIndex++},
-            {text: 'three', image: '/img/艺术.png', id: currIndex++},
-            {text: 'four', image: '/img/投资.png', id: currIndex++},
+            {text: 'one', image: '/img/jiujiao.png', id: currIndex++},
+            {text: 'two', image: '/img/lvxing.png', id: currIndex++},
+            {text: 'three', image: '/img/yishu.png', id: currIndex++},
+            {text: 'four', image: '/img/touzi.png', id: currIndex++},
         ];
         
 	});
